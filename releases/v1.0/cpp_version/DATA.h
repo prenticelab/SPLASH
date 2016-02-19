26c152dfe8ea54ab99f6bda9da8210b99c09a776
twdavis <twd34@cornell.edu>
2016-02-19 14:53:15 -0500

    Copyright (C) 2016 Prentice Lab

--- releases/v1.0/cpp_version/DATA.h
+++ releases/v1.0/cpp_version/DATA.h
@@ -5,12 +5,27 @@
  * DATA.h
  *
  * VERSION 1.0
- * LAST UPDATED: 2016-02-16
+ * LAST UPDATED: 2016-02-19
  *
  * ~~~~~~~~
  * license:
  * ~~~~~~~~
- * Copyright (C) 2015, see LICENSE
+ * Copyright (C) 2016 Prentice Lab
+ *
+ * This file is part of the SPLASH model.
+ *
+ * SPLASH is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * SPLASH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SPLASH.  If not, see <http://www.gnu.org/licenses/>.
  *
  * ~~~~~~~~~
  * citation:
