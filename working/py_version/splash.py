a7a0cce7e3fcf30b1c27f17d660fc57c2b0e851f
Rebecca Thomas <r.thomas14@imperial.ac.uk>
2016-04-26 13:36:33 +0100

    changes to splash ro

--- working/py_version/splash.py
+++ working/py_version/splash.py
@@ -341,8 +341,8 @@ class SPLASH:
         
         self.bucket_full_idx = numpy.where(self.sm > kWm)
         self.bucket_empty_idx = numpy.where(self.sm < 0)
-        #self.other_bucket_idx = numpy.where(((self.sm > kWm) & (self.sm <  numpy.float64(0.0)))
-        #                                 | numpy.isnan(self.sm))
+        self.other_bucket_idx = numpy.where(((self.sm > kWm) & (self.sm <  numpy.float64(0.0)))
+                                         | numpy.isnan(self.sm))
 
         #self.sm = 0.0
         #self.ro = 0.0
