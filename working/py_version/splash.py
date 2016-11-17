7f309aafedb207e6a0c4c18234455776ffe47ff9
Rebecca <r.thomas14@imperial.ac.uk>
2016-11-17 18:48:54 +0000

    Getting raidaiotn out

--- working/py_version/splash.py
+++ working/py_version/splash.py
@@ -335,6 +335,7 @@ class SPLASH:
             self.eet = self.evap.eet_d    # daily equilibrium ET, mm
             self.pet = self.evap.pet_d    # daily potential ET, mm
             self.ppfd_d = self.evap.ppfd_d
+            self.hn = self.evap.hn #Daily total radiation
 
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # 3. Calculate today's soil moisture (sm), mm
