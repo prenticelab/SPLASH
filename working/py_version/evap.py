941f77a5f8e576eff3c0588c39c14260a4e6fd4a
Rebecca <r.thomas14@imperial.ac.uk>
2016-11-22 17:39:34 +0000

    Minor edits from site level comparison

--- working/py_version/evap.py
+++ working/py_version/evap.py
@@ -307,6 +307,7 @@ class EVAP:
         """
         self.logger.debug("estimating atmospheric pressure at %f m", nanmean(z))
         p = kPo*(1.0 - kL*z/kTo)**(kG*kMa/(kR*kL))
+        #p = kPo*(1.0 - kL*z/kTo)**(kG*kMa/(8.3143*kL))
         return p
 
     def density_h2o(self, tc, p):
@@ -367,6 +368,74 @@ class EVAP:
         pw /= (ko + ca*pbar + cb*pbar**2.0 - pbar)
         pw *= (1e3)*po
         return pw
+
+    def viscosity_h2o(self, tc, p):
+        """
+        Name:     LUE.viscosity_h2o
+        Input:    - float, ambient temperature (tc), degrees C
+                  - float, ambient pressure (p), Pa
+        Return:   float, viscosity of water (mu), Pa s
+        Features: Calculates viscosity of water at a given temperature and
+                  pressure.
+        Depends:  density_h2o
+        Ref:      Huber, M. L., R. A. Perkins, A. Laesecke, D. G. Friend, J. V.
+                  Sengers, M. J. Assael, ..., K. Miyagawa (2009) New
+                  international formulation for the viscosity of H2O, J. Phys.
+                  Chem. Ref. Data, Vol. 38(2), pp. 101-125.
+        """
+        # Define reference temperature, density, and pressure values:
+        tk_ast = 647.096      # Kelvin
+        rho_ast = 322.0       # kg/m^3
+        mu_ast = (1e-6)       # Pa s
+
+        # Get the density of water, kg/m^3
+        rho = self.density_h2o(tc, p)
+
+        # Calculate dimensionless parameters:
+        tbar = (tc + 273.15)/tk_ast
+        tbarx = tbar**(0.5)
+        tbar2 = tbar**2
+        tbar3 = tbar**3
+        rbar = rho/rho_ast
+
+        # Calculate mu0 (Eq. 11 & Table 2, Huber et al., 2009):
+        mu0 = 1.67752
+        mu0 += 2.20462/tbar
+        mu0 += 0.6366564/tbar2
+        mu0 += -0.241605/tbar3
+        mu0 = 1e2*tbarx/mu0
+
+        # Create Table 3, Huber et al. (2009):
+        hj0 = (0.520094, 0.0850895, -1.08374, -0.289555, 0., 0.)
+        hj1 = (0.222531, 0.999115, 1.88797, 1.26613, 0., 0.120573)
+        hj2 = (-0.281378, -0.906851, -0.772479, -0.489837, -0.257040, 0.)
+        hj3 = (0.161913,  0.257399, 0., 0., 0., 0.)
+        hj4 = (-0.0325372, 0., 0., 0.0698452, 0., 0.)
+        hj5 = (0., 0., 0., 0., 0.00872102, 0.)
+        hj6 = (0., 0., 0., -0.00435673, 0., -0.000593264)
+        h = hj0 + hj1 + hj2 + hj3 + hj4 + hj5 + hj6
+        h_array = numpy.reshape(numpy.array(h), (7, 6))
+
+        # Calculate mu1 (Eq. 12 & Table 3, Huber et al., 2009):
+        mu1 = 0.
+        ctbar = (1./tbar) - 1.
+        for i in range(6):
+            coef1 = numpy.power(ctbar, i)
+            coef2 = 0.
+            for j in range(7):
+                coef2 += h_array[j][i]*numpy.power((rbar - 1.), j)
+            mu1 += coef1*coef2
+        mu1 = numpy.exp(rbar*mu1)
+
+        # Calculate mu_bar (Eq. 2, Huber et al., 2009)
+        #   assumes mu2 = 1
+        mu_bar = mu0*mu1
+
+        # Calculate mu (Eq. 1, Huber et al., 2009)
+        mu = mu_bar*mu_ast    # Pa s
+
+        return mu
+
 
     def psychro(self, tc, p):
         """
